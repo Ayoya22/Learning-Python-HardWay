7d481eaf01419c43c0d3d8d14625715c9f6bb47d
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-15 14:10:04 +0100

    Simple python code to show how mathematical symbols are handled

---
+++ ex3-number_maths.py
@@ -0,0 +1,5 @@
+print("This is a mathematics operation", 5 + 3 / 2 - 4 * 6 % 5)
+print('Is 5 greater than 2?', 5>2)
+print("-7 is greater than 5", -7 > 5 )
+print("8 is less than or equal to -10?", 8 <= -10)
+#Simple program to show how python handles mathematical symbols.

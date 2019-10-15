87f56c5c69a1380980fa69e7f499014245a2554b
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-15 13:48:15 +0100

    This file is to show how simple comments work

---
+++ ex2-comments.py
@@ -0,0 +1,6 @@
+# This is a comment, it is being ignored by the compiler
+print('This is a text ')
+#print('when you type # at the begining of a line it will comment out the whole line')
+print("When you type '#' in a string it is printed out ")
+print("thi is a text")#when you type it outside a string it will comment out the rest of that line
+print('The end of this program')

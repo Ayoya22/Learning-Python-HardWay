ad48932e3ac635930f2960f9cffa83d50a7d6a40
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-15 12:16:15 +0100

    First program that prints out text on screen

---
+++ ex1-printing.py
@@ -0,0 +1,5 @@
+print('This is my first program')
+print('I have to learn it everyday')
+print('I must finish the book in less than a month')
+print('Then move on to the front-end development')
+print('I want to do machine Learning')

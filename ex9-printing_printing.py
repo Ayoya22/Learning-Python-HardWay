d1c405aaea41ce5bf566ef0fccb4a43fabad1665
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-16 05:26:16 +0100

    block printing and more printing

---
+++ ex9-printing_printing.py
@@ -0,0 +1,11 @@
+PL = "Python, C++, JavaScript, Java,"
+phone_brands = "xiaomi, apple, 1+, samsung, huawei, oppo"
+
+print(f"This are some of the most popular programming languages: ", PL)
+print("This are some of the most popular phone brands: ", phone_brands )
+print(''' # this line will not be commented out becuse it is embeded in the block print 
+    This is a multiline print 
+    It is very simple to make as you can see
+    Just need to type in the triple quotes and that's all
+    Python is an awesome language.
+''')# this line is commented out because it is not in the block printing.

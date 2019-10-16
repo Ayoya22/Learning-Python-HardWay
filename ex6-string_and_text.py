0665b89cb84d75ac08edaee7680ca51fd056944b
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-16 04:30:14 +0100

    strings , text and formatting

---
+++ ex6-string_and_text.py
@@ -0,0 +1,12 @@
+num_sdt = 50
+sdt1 = f"Good Morining Sir!"
+res = f"Fine thank you"
+tch = f"Good Moring Class. How are you today? {res}"
+
+ques_tch = "Is your class clean? {}"
+sdt_res = True
+
+print(sdt1)
+print(tch)
+print(ques_tch.format(sdt_res))
+print(f"How many students are in tis class? {num_sdt}")

b9f3348e2e5337b7fefe69f6c0d0511aa92e1924
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-16 04:56:23 +0100

    More on strings and thier manipulation

---
+++ ex7-more_printing.py
@@ -0,0 +1,18 @@
+print('I am a software developer')
+print('and I am also a {}'.format('christian'))
+print("I love Jesus " * 5)
+
+let1 = 'J'
+let2 = 'E'
+let3 = 'S'
+let4 = 'U'
+let5 = 'S'
+let6 = 'C'
+let7 = 'H'
+let8 = 'R'
+let9 = 'I'
+let10 = 'S'
+let11 = 'T'
+
+print(let1 + let2 + let3 + let4 + let5, end =' ' )
+print(let6 + let7 + let8 + let9 + let10 + let11)

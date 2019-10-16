72732125e6dafa4bd09629443175390584d8f19d
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-16 08:01:37 +0100

    some common escape characters in python

---
+++ ex10-escape_character.py
@@ -0,0 +1,14 @@
+text1 = "\tI'm a software developer and I'll tab in this text" # the /t will yab the text
+text2 = "I will us the backslach-n\n to split this phrase" # the /n will take the following character to the new line.
+text4 = 'I \\ am \\ cool' # This will just separate the words using a slash
+text3 = '''
+    This is a list of tabbed items:
+    \t - love,
+    \t - hardwork,
+    \t - discipline,
+    \t - prayer \n\t - respect
+''' #Block text 
+print(text1)
+print(text2)
+print(text3)
+print(text4)

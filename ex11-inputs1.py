2e5fd4ad648b0da43444f40465607fce962d835b
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-16 08:26:41 +0100

    Added syntax to convert from string to int with some comments.

--- ex11-inputs1.py
+++ ex11-inputs1.py
@@ -1,6 +1,6 @@
 #This program is just to show how to collect data from a user into a python program
 
-print('What is your name?', end =" ")
+print('What is your name?', end =" ") #By default the input is taken as a string
 name = input()
 
 print ('Please, type in your address..', end =' ')
@@ -8,5 +8,10 @@ address = input()
 
 print('Lastly, what is your profession?', end =' ')
 profession = input()
+
+print('Please tell me your age in 2019: ', end = ' ')
+age = int(input()) # I used the int() to convert it into an integer to carry on mathematical operations.
+year = 2019 - age
+print(f'This means you were born in the year {year}')
 
 print(f'Okay! If I got you well, your name is {name}, you are a {profession} and you leave at {address}.')

65975354ff14061b00121671f70deff87be7a017
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-16 03:34:03 +0100

    This demonstrate a variable embeded in a string

---
+++ ex5-var_in_strings.py
@@ -0,0 +1,13 @@
+#In th efollowing line I will initialiuse the variables in order to use them embeded in the strings
+my_name = 'Djoukam'
+my_age = 27 #this is a lie
+weight = 67 # am not very sure of this
+room = 452
+town = 'Yaounde'
+address = 'Ayene - Catholic University'
+total = my_age + weight + room
+
+print(f"This is to format the data(the text)")
+print(f"My name is {my_name} and I weigh {weight} kg")
+print(f"and I am {my_age} years old, I live at {address} in room number: {room}.")
+print(f"If I add {my_age} and {weight} together with {room} I will obtain {total}")

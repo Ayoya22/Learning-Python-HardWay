5233a4e385d13fc989e51891119adcccf52bc093
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-16 04:59:50 +0100

    Added comments to the code for more understanding

--- ex7-more_printing.py
+++ ex7-more_printing.py
@@ -1,6 +1,6 @@
 print('I am a software developer')
-print('and I am also a {}'.format('christian'))
-print("I love Jesus " * 5)
+print('and I am also a {}'.format('christian')) #Here I formatted the word 'christian' since it was not in the first place. 
+print("I love Jesus " * 5) #Do you know what this will do? It will multiply the string five times
 
 let1 = 'J'
 let2 = 'E'
@@ -14,5 +14,5 @@ let9 = 'I'
 let10 = 'S'
 let11 = 'T'
 
-print(let1 + let2 + let3 + let4 + let5, end =' ' )
+print(let1 + let2 + let3 + let4 + let5, end =' ' ) # the 'end' is a keyword in python we will look at it in more details later
 print(let6 + let7 + let8 + let9 + let10 + let11)

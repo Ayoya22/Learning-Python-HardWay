a28f7c5bef0199539a691548339301f8797a33d7
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-19 21:03:51 +0100

    Completed python script

--- ex13-sys_arg.py
+++ ex13-sys_arg.py
@@ -1,6 +1,9 @@
 #This is a program to show you how to use the system parameters
 from sys import argv
 
-filename, one, two, third = argv
+file, one, two, third = argv
 
-
+print("this is the name of the files", file)
+print("this is an argv", one)
+print("this is an argv", two)
+print("this is an argv", third)

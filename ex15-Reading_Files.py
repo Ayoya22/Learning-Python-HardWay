d0c5d32baa3014e2287e70799149504c94d7f18c
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-20 19:24:32 +0100

    Code for reading text files using both the 'argv' and 'input()'.

---
+++ ex15-Reading_Files.py
@@ -0,0 +1,14 @@
+#In this program we will use python to read text files
+#This code presents two methods, either through 'input()' or through 'argv'
+from sys import argv
+
+filename , textfile = argv
+#We have to open the file first before trying to read it.
+text = open(textfile)
+print(f'This is your text file {textfile} and this is what it contains:')# We are using argv method
+print(text.read()) #here we read the file and print it out.
+
+print('Pleease enter your text file again..')
+textfile2 = input()#Here we are using the input method.
+text2 = open(textfile2)
+print(text2.read())

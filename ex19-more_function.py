51c70dccb7db9d678d3097174418d8fd433bea5b
Ayoya22 <djoukamsimplice@gmail.com>
2019-10-26 20:45:44 +0100

    Completed the function.

--- ex19-more_function.py
+++ ex19-more_function.py
@@ -10,7 +10,11 @@ cheese_and_crackers(20, 30)
 print("OR, we can use variables from our script:")
 amount_of_cheese = 10
 amount_of_crackers = 50
+
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
 print("We can even do math inside too:")
 cheese_and_crackers(10 + 20, 5 + 6)
+
 print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)

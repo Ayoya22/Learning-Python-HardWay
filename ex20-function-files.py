8211809989d3d81cfebeb36f3ae89a0c3523ef61
Ayoya22 <djoukamsimplice@gmail.com>
2019-11-01 22:21:39 +0100

    Corrected more errors related to identation.

--- ex20-function-files.py
+++ ex20-function-files.py
@@ -1,11 +1,11 @@
 from sys import argv
 script, input_file = argv
 def print_all(f):
-print(f.read())
+    print(f.read())
 def rewind(f):
-f.seek(0)
+    f.seek(0)
 def print_a_line(line_count, f):
-print(line_count, f.readline())
+    print(line_count, f.readline())
 current_file = open(input_file)
 print("First let's print the whole file:\n")
 print_all(current_file)

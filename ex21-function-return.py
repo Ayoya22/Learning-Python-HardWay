6cb7b22e05928742929314c5590be88cb70e549a
Ayoya22 <djoukamsimplice@gmail.com>
2019-11-19 03:48:30 +0100

    Added the input myself so we can obtain some data from the user

--- ex21-function-return.py
+++ ex21-function-return.py
@@ -5,3 +5,7 @@ def add(a,b,c):
 def div(a,b):
     print(f"Dividing {a} by {b}")
     return a/b
+print("You will have to enter 3 numbers, they will all be added then the first one will be divided by the sencond and you'll se the results")
+a =input("Enter the first number : ")
+b =input("Enter the second number : ")
+c =input("Enter the third number : ")

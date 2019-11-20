f6c817cca5405a049e99029655570c8e28c40044
Ayoya22 <djoukamsimplice@gmail.com>
2019-11-20 08:49:04 +0100

    Completed the file to print results

--- ex21-function-return.py
+++ ex21-function-return.py
@@ -6,6 +6,12 @@ def div(a,b):
     print(f"Dividing {a} by {b}")
     return a/b
 print("You will have to enter 3 numbers, they will all be added then the first one will be divided by the sencond and you'll se the results")
-a =input("Enter the first number : ")
-b =input("Enter the second number : ")
-c =input("Enter the third number : ")
+x =input("Enter the first number : ")
+y =input("Enter the second number : ")
+z =input("Enter the third number : ")
+
+result1 = add(x,y,z)
+result2 = div(x,y)
+
+print(f"The result of addition is: {result1}")
+print(f"The result of division is: {result2}")

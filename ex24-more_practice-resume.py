3749244ad181fd9bbb3cd5acfe76b6da06517205
Ayoya22 <djoukamsimplice@gmail.com>
2019-11-23 22:49:39 +0100

    taken care of the things concerning escape characters.

---
+++ ex24-more_practice-resume.py
@@ -0,0 +1,4 @@
+# In this program we will apply most of the interesting things we have learned so far
+print("Let's go through most of the cool stuff")
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.')
